45ab1108a694eaed757f5399269df81512e27eca
Your Name <you@example.com>
2020-09-09 21:53:44 -0500

    FInished Project

--- index.js
+++ index.js
@@ -39,9 +39,25 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
+function Person(name ,age) {
+    this.name = name
+    this.age =age
+    this.stomach = []
 
 }
+
+Person.prototype.eat = function(e){
+  if(this.stomach.length < 10){
+    this.stomach.push(e);
+  }
+}
+Person.prototype.poop = function(){
+   this.stomach = []
+}
+Person.prototype.toString = function (){
+  return `${this.name}, ${this.age}`
+}
+
 
 /*
   TASK 2
@@ -57,8 +73,27 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
+function Car(model,milesPerGallon) {
+  this.tank = 0;
+  this.odometer = 0
+  this.model =model;
+  this.milesPerGallon = milesPerGallon;
 
+}
+Car.prototype.fill = function (gallons){
+  this.tank += gallons;
+}
+
+Car.prototype.drive = function (distance){
+  let fuel = distance/this.milesPerGallon
+  if(this.tank >= fuel){
+    this.odometer += distance
+    this.tank -= fuel;
+  }else {
+    this.odometer+=(this.milesPerGallon*this.tank)
+    this.tank =0;
+    return `I ran out of fuel ${this.odometer} miles`
+  }
 }
 
 /*
@@ -68,18 +103,26 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name , age , favoriteToy) {
+  this.name = name;
+  this.age = age;
+  this.favoriteToy = favoriteToy
+}
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return `Playing with, ${this.favoriteToy}`
 }
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1.when this is used in the global object the window and console will be the value
+  2. when you use implicit binding the left of the dot is refering to the object.
+  3. if a function was called with new , the this keyword is referencing that new object that was created.
+  4. when we use call the this word is defined.
 */
 
 
